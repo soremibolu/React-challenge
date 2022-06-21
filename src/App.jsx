f718ece8e83703baa4e2e7d182b566baafb25a4c
Bolu Soremi <soremibolu@gmail.com>
2022-06-21 19:07:02 +0100

    adding details /details route to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Details from "./pages/Details";
 import "./global.css"
 
 /**
@@ -18,6 +19,7 @@ class App extends Component{
             <section>
                 <Routes>
                   <Route path={"/"} element={<Home />} />
+                  <Route path={"/Details"} element={<Details />} />
                 </Routes>
             </section>
           </main>
